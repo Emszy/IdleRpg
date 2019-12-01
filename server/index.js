622407a966c8b75f570da0921af61c10868119c7
Emszy <bucheiteric@gmail.com>
2019-11-30 21:15:24 -0800

    Changed server start method

--- server/index.js
+++ server/index.js
@@ -40,12 +40,11 @@ armorRoute(app, db);
 
 
 
-app.get('/', (req, res) => {
-  res.json(start: "APP STARTED");
+app.get('*', (req, res) => {
 });
 
 
 
-app.listen(process.env.PORT || 3001, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
