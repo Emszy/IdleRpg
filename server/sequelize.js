eb840d9ddbf868e4663dbe97d53994ba585e2457
Emszy <bucheiteric@gmail.com>
2019-11-30 23:05:57 -0800

    added heroku pg setup

--- server/sequelize.js
+++ server/sequelize.js
@@ -5,16 +5,25 @@ const {seed} = require("./seed");
 var database = process.env.DATABASE_URL || 'idle_forest'
 
 
-const sequelize = new Sequelize(database, 'postgres', 'postgres', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-})
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+  })
+} else {
+  const sequelize = new Sequelize(database, 'postgres', 'postgres', {
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  })  
+}
+
 
 
 const db = {};
